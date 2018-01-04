f6c2a488016db8fe354eb2250f06186ec1709402
Ken Mickles <ken@37i.net>
2018-01-04 14:59:14 -0500

    Add Facebook and Twitter buttons
    + some HTML cleanup

--- js/app.js
+++ js/app.js
@@ -207,6 +207,44 @@ document.addEventListener("DOMContentLoaded", function() {
             self.formMessage = "That didn't work for some reason :(";
           }
         });
+      },
+
+      getMetaContent: function(name) {
+        var el = document.querySelector('meta[name="' + name + '"]') || document.querySelector('meta[property="' + name + '"]');
+        
+        if (el) {
+          return el.getAttribute('content');
+        }
+
+        return null;
+      },
+
+      openPopup: function(url, title='popup', w=600, h=500) {
+        // Fixes dual-screen position
+        var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+        var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+        var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+        var top = ((height / 2) - (h / 2)) + dualScreenTop;
+        var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+        // Puts focus on the newWindow
+        if (window.focus) {
+          newWindow.focus();
+        }
+      },
+
+      shareOnFacebook: function() {
+        var url = this.getMetaContent('og:url');
+        this.openPopup('https://www.facebook.com/sharer.php?u=' + encodeURIComponent(url), 'facebook');
+      },
+
+      shareOnTwitter: function() {
+        var tweetText = this.getMetaContent('twitter:description') + ' ' + this.getMetaContent('twitter:url');
+        this.openPopup('https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweetText), 'twitter');
       }
     }
   });
